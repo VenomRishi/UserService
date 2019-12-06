c015decccdf2ce0a6b6bd3fcd3add85290ce4427
VenomRishi <mhatrerishikesh@gmail.com>
2019-12-06 19:35:06 +0530

    Eureka microservice is implemented now this project will going to maintain in other repository(fundoonotebackend)

--- src/main/java/com/bridgelabz/fundoo/user/UserServiceApplication.java
+++ src/main/java/com/bridgelabz/fundoo/user/UserServiceApplication.java
@@ -15,9 +15,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableCaching
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
